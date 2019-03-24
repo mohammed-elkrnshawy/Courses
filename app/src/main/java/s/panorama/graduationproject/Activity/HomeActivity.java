58c58b681f2878b42d3cce2c76b674cf6821d909
omniaelbasuoni <omniasamiir18@gmail.com>
2019-03-24 21:50:39 +0200

    first commit

---
+++ app/src/main/java/s/panorama/graduationproject/Activity/HomeActivity.java
@@ -0,0 +1,15 @@
+package s.panorama.graduationproject.Activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import s.panorama.graduationproject.R;
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+}
