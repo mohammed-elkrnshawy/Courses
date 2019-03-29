97a667c0c5ac8d47e66927d8b437e47654189fd5
mohammedel_elkrnshawy <mohammedelkrnshawy@gmail.com>
2019-03-29 21:35:36 +0200

    'Login'

---
+++ app/src/main/java/s/panorama/graduationproject/Activity/ForgetActivity.java
@@ -0,0 +1,15 @@
+package s.panorama.graduationproject.Activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import s.panorama.graduationproject.R;
+
+public class ForgetActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_forget);
+    }
+}
--- app/src/main/java/s/panorama/graduationproject/Activity/LoginActivity.java
+++ app/src/main/java/s/panorama/graduationproject/Activity/LoginActivity.java
@@ -1,15 +1,52 @@
 package s.panorama.graduationproject.Activity;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+import butterknife.OnItemClick;
 import s.panorama.graduationproject.R;
 
 public class LoginActivity extends AppCompatActivity {
+
+    @BindView(R.id.txtRegister)
+    TextView txtRegister;
+    @BindView(R.id.btnLogin)
+    Button btnLogin;
+    @BindView(R.id.txtForget)
+    TextView txtForget;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        ButterKnife.bind(this);
+
+
     }
+
+    @OnClick({R.id.txtRegister}) void onButtonClick (View view) {
+        switch (view.getId()) {
+            case R.id.txtRegister:
+                startActivity(new Intent(LoginActivity.this,RegisterActivity.class));
+                break;
+            case R.id.txtForget:
+                startActivity(new Intent(LoginActivity.this,ForgetActivity.class));
+                break;
+            case R.id.btnLogin:
+                validateData();
+                break;
+        }
+    }
+
+    private void validateData() {
+    }
+
 }
