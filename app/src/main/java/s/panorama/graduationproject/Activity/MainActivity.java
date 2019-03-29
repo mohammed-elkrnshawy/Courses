626dd302dcbba90f13c1f2c6dd51aa3498c5fc1b
mohammedel_elkrnshawy <mohammedelkrnshawy@gmail.com>
2019-03-29 21:07:14 +0200

    'Login'

--- app/src/main/java/s/panorama/graduationproject/Activity/MainActivity.java
+++ app/src/main/java/s/panorama/graduationproject/Activity/MainActivity.java
@@ -1,5 +1,7 @@
 package s.panorama.graduationproject.Activity;
 
+import android.content.Intent;
+import android.os.CountDownTimer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -11,5 +13,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        readData();
+
+    }
+
+    private void readData() {
+        new CountDownTimer(3000, 1000) {
+
+            public void onTick(long millisUntilFinished) {
+            }
+
+            public void onFinish() {
+                startActivity(new Intent(MainActivity.this,LoginActivity.class));
+                finishAffinity();
+            }
+        }.start();
     }
 }
